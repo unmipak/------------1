75c963132738ce6da408c1778715b0b3760ed61d
Маргарита Пак <paktoka@gmail.com>
2023-07-19 23:10:15 +0300

    Акселератор день 3: слайдер, спрайт

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,4 +1,14 @@
 // Swiper 7.4.1
-import './vendor/swiper';
-import './vendor/focus-visible-polyfill';
+import Swiper, {Navigation, Pagination} from './vendor/swiper';
+// import './vendor/focus-visible-polyfill';
+
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 

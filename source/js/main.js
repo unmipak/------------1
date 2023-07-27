74c3eb192b874eb3fa314f1d49114940aba65b8d
Маргарита Пак <paktoka@gmail.com>
2023-07-28 00:07:13 +0300

    Акселератор день 9

--- source/js/main.js
+++ source/js/main.js
@@ -4,11 +4,16 @@ import {Form} from './modules/form-validate/form';
 import {initAccordions} from './vendor/init-accordion';
 import {initTabs} from './vendor/init-tabs';
 
+
+// аккордеон
+
 window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     initAccordions();
   });
 });
+
+// табы
 
 window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
@@ -16,20 +21,12 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// ---------------------------------
+// валидация формы
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  // Utils
-  // ---------------------------------
-
   iosVhFix();
 
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
     const form = new Form();
@@ -38,27 +35,25 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// ---------------------------------
+// видео
 
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+let video = document.querySelector('.gym__video');
+let videoBtn = document.querySelector('.gym__video-btn');
 
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
+function videoBtnOnclick() {
+  if (video.paused) {
+    video.play();
+    videoBtn.style.display = 'none';
+  } else {
+    video.pause();
+  }
+}
 
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
+function videoOnclick() {
+  video.pause();
+  videoBtn.style.display = 'block';
+}
 
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
+video.addEventListener('click', videoOnclick);
+videoBtn.addEventListener('click', videoBtnOnclick);
 
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
